796317a29177201ed4beac0f882578809616bdc7
18upDEV <gobgob04@gmail.com>
2025-03-13 18:49:03 +0700

    Updatev3

--- src/pages/GameRoom.vue
+++ src/pages/GameRoom.vue
@@ -91,8 +91,12 @@ function startGame() {
   </div>
 
   <div v-for="player of players" :key="player.peer" class="flex w-full max-w-xs items-center gap-4">
-    <svg v-if="player.isRoomLeader" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 256 256"><path fill="#cfcfcf" d="M243.84 76.19a12.08 12.08 0 0 0-13.34-1.7l-50.21 25l-41.[...]
-    <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#cfcfcf" d="M12 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 2a2 2 0 0 0-2 [...]
+    <svg v-if="player.isRoomLeader" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 256 256">
+      <path fill="#cfcfcf" d="M243.84 76.19a12.08 12.08 0 0 0-13.34-1.7l-50.21 25l-41.2 20.6l-44.95-19.3a12.1 12.1 0 0 0-11.27 0L3.5 119.19a12.07 12.07 0 0 0-6.05 14.59a12.07 12.07 0 0 0 14.58 6.05l50.21-25l41.2-20.6l44.95 19.3a12.1 12.1 0 0 0 11.27 0L252.5 96.39a12.07 12.07 0 0 0 6.05-14.59a12.07 12.07 0 0 0-14.58-6.05l-50.21 25l-41.2 20.6l-44.95-19.3a12.1 12.1 0 0 0-11.27 0L3.5 119.19a12.07 12.07 0 0 0-6.05 14.59a12.07 12.07 0 0 0 14.58 6.05l50.21-25l41.2-20.6l44.95 19.3a12.1 12.1 0 0 0 11.27 0L252.5 96.39a12.07 12.07 0 0 0 6.05-14.59a12.07 12.07 0 0 0-14.58-6.05l-50.21 25l-41.2 20.6l-44.95-19.3a12.1 12.1 0 0 0-11.27 0L3.5 119.19a12.07 12.07 0 0 0-6.05 14.59a12.07 12.07 0 0 0 14.58 6.05l50.21-25l41.2-20.6l44.95 19.3a12.1 12.1 0 0 0 11.27 0L252.5 96.39a12.07 12.07 0 0 0 6.05-14.59a12.07 12.07 0 0 0-14.58-6.05"/>
+    </svg>
+    <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <path fill="#cfcfcf" d="M12 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 2a2 2 0 0 0-2 2a2 2 0 0 0 2 2a2 2 0 0 0 2-2a2 2 0 0 0-2-2m0 10c5.33 0 8 2.67 8 8H4c0-5.33 2.67-8 8-8z"/>
+    </svg>
 
     <span>{{ player.playerName }}</span>
     <span v-if="!player.isRoomLeader" class="text-primary text-opacity-40">({{ player.peer }})</span>
